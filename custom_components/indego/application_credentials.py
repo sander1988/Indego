8ab708c2f265271c203a55fbed0ba46c42192c81
Sander Peterse <sander.peterse88@gmail.com>
2023-03-13 21:06:50 +0100

    Implemented auto creation of oauth credentials.

--- custom_components/indego/application_credentials.py
+++ custom_components/indego/application_credentials.py
@@ -5,14 +5,14 @@ from homeassistant.components.application_credentials import AuthorizationServer
 from homeassistant.helpers import config_entry_oauth2_flow
 from homeassistant.core import HomeAssistant
 
-
 async def async_get_auth_implementation(
     hass: HomeAssistant, auth_domain: str, credential: ClientCredential
 ) -> config_entry_oauth2_flow.AbstractOAuth2Implementation:
     """Return custom auth implementation."""
 
     # Secret needs to be unset or auth fails.
-    # GUI always sets the value; so clear it before use.
+    # But ClientCredential() fail to store when it's set to None.
+    # So we store "" in the config_flow.py and clear it here before use.
     credential.client_secret = None
 
     return IndegoLocalOAuth2Implementation(
--- custom_components/indego/config_flow.py
+++ custom_components/indego/config_flow.py
@@ -7,13 +7,15 @@ from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import selector
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.components.application_credentials import ClientCredential, async_import_client_credential, DOMAIN as AC_DOMAIN, DATA_STORAGE as AC_DATA_STORAGE
 
 from pyIndego import IndegoAsyncClient
 
 from .const import (
     DOMAIN,
     CONF_MOWER_SERIAL,
-    CONF_MOWER_NAME
+    CONF_MOWER_NAME,
+    OAUTH2_CLIENT_ID
 )
 
 from homeassistant.const import (
@@ -30,7 +32,6 @@ DATA_SCHEMA: Final = vol.Schema(
 
 _LOGGER: Final = logging.getLogger(__name__)
 
-# TODO: Add default OAuth client ID (is the same for all users).
 # TODO: Reimplement mower selection in after auth.
 
 
@@ -51,6 +52,31 @@ class IndegoFlowHandler(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, doma
     def extra_authorize_data(self) -> dict:
         """Extra data that needs to be appended to the authorize url."""
         return {"scope": "openid profile email offline_access https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User"}
+
+    async def async_step_user(
+            self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle a flow start."""
+
+        # Create the OAuth application credentials entry in HA.
+        # No need to ask the user for input, settings are the same for everyone.
+        credentials = self.hass.data[AC_DOMAIN][AC_DATA_STORAGE].async_client_credentials(DOMAIN)
+        if DOMAIN not in credentials:
+            try:
+                _LOGGER.debug("Application credentials do NOT exist, creating...")
+                await async_import_client_credential(
+                    self.hass,
+                    DOMAIN,
+                    ClientCredential(OAUTH2_CLIENT_ID, "", DOMAIN)
+                )
+                _LOGGER.debug("OK: Imported OAuth client credentials")
+            except Exception as exc:
+                _LOGGER.error("Failed to create application credentials! Reason: %s", str(exc))
+                raise
+        else:
+            _LOGGER.debug("Application credentials found, NOT creating")
+
+        return await super().async_step_pick_implementation(user_input)
 
     # async def async_step_user(
     #     self, user_input: dict[str, Any] | None = None
